bf497a88f1cb4e47dc9cc3a57708ac7ac8a3a931
dangtrantrung <trung280584@gmail.com>
2024-01-14 17:37:28 +0700

    Render action

--- Q Learning.py
+++ Q Learning.py
@@ -1,7 +1,17 @@
 import gym
 # Environment
-env=gym.make("MountainCar-v0",render_mode="rgb_array")
+env=gym.make("MountainCar-v0",render_mode='human')
 env.reset()
 print(env.state)
-env.render()
-input()
+# obs = env.render()
+# input()
+# Take actions
+print(env.action_space.n)
+# Take X range, V range
+print(env.observation_space.high)
+print(env.observation_space.low)
+# Render
+while True:
+    action=2
+    env.step(action)
+    env.render()
