ec6dc546ace13c86800f948ec5fe7b46416dc530
dangtrantrung <trung280584@gmail.com>
2024-01-20 17:20:58 +0700

    Update Q_table

--- Q Learning_2.py
+++ Q Learning_2.py
@@ -13,24 +13,28 @@ def convert_state(real_state):
     return tuple(q_state.astype(np.int))
 
 q_table=np.random.uniform(low=-2,high=0,size=(q_table_size+[env.action_space.n]))
-done=False
-current_state=convert_state(env.reset())
+c_no_of_eps=10000
+for ep in range(c_no_of_eps):
+    print("Eps= ",ep)
+    done=False
+    current_state=convert_state(env.reset())
 
-while not done:
-   # get current argmax Q value of current state
-   action=np.argmax(q_table[current_state])
-   # Take next action
-   next_real_state,reward,done, _ =env.step(action=action)
-   if done:
-        if next_real_state[0]>env.goal_position:
-            print("Reach goal")
+    while not done:
+        # get current argmax Q value of current state
+        action=np.argmax(q_table[current_state])
+        # Take next action
+        next_real_state,reward,done, _ =env.step(action=action)
+        if done:
+                if next_real_state[0]>env.goal_position:
+                    print("Reach goal at ep=",ep)
+                else:
+                    print("Failed")
+            #...
         else:
-            print("Failed")
-    #...
-   else:
-        #convert to q_state
-        next_state=convert_state(next_real_state)
-        # Update Q value for current_state,action
-        current_q_value=q_table[current_state+(action,)]
-        new_q_value=(1-c_learning_rate)*current_q_value+c_learning_rate*(reward+c_discount_value*np.max(q_table[next_state]))
-        current_state=next_state
+                #convert to q_state
+                next_state=convert_state(next_real_state)
+                # Update Q value for current_state,action
+                current_q_value=q_table[current_state+(action,)]
+                new_q_value=(1-c_learning_rate)*current_q_value+c_learning_rate*(reward+c_discount_value*np.max(q_table[next_state]))
+                q_table[current_state+(action,)]=new_q_value
+                current_state=next_state
