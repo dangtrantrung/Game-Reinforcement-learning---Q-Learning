21ad29fbb9166845e42d69102d3b61cf3d1520a6
dangtrantrung <trung280584@gmail.com>
2024-01-21 10:42:18 +0700

    Print Results

--- Q Learning_2.py
+++ Q Learning_2.py
@@ -14,19 +14,36 @@ def convert_state(real_state):
 
 q_table=np.random.uniform(low=-2,high=0,size=(q_table_size+[env.action_space.n]))
 c_no_of_eps=10000
+c_show_each=1000
+max_ep_reward=-999
+max_ep_action_list=[]
+
 for ep in range(c_no_of_eps):
     print("Eps= ",ep)
     done=False
     current_state=convert_state(env.reset())
+    ep_reward=0
+    action_list=[]
+    if ep%c_show_each==0:
+        show_now=True
+    else:
+        show_now=False
 
     while not done:
         # get current argmax Q value of current state
         action=np.argmax(q_table[current_state])
+        action_list.append(action)
         # Take next action
         next_real_state,reward,done, _ =env.step(action=action)
+        ep_reward+=reward
+        if show_now:
+            env.render()
         if done:
                 if next_real_state[0]>env.goal_position:
-                    print("Reach goal at ep=",ep)
+                    print("Reach goal at ep={}, reward={}",format(ep,ep_reward))
+                    if ep_reward>max_ep_reward:
+                        max_ep_reward=ep_reward
+                        max_ep_action_list=action_list
                 else:
                     print("Failed")
             #...
@@ -38,3 +55,6 @@ for ep in range(c_no_of_eps):
                 new_q_value=(1-c_learning_rate)*current_q_value+c_learning_rate*(reward+c_discount_value*np.max(q_table[next_state]))
                 q_table[current_state+(action,)]=new_q_value
                 current_state=next_state
+# Print Results
+print("Max reward= ",max_ep_reward)
+print("Max action= ",max_ep_action_list)
